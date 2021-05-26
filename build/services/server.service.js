1805e14356f3bf9f7e6eb46fc1885646f25e9143
pollajoel <pollajoel2017@gmail.com>
2021-05-26 09:00:48 +0200

    add to heroku

--- build/services/server.service.js
+++ build/services/server.service.js
@@ -2,15 +2,15 @@ const config = require("../configs")
 const PORT = process.env.PORT
 const express = require("express")
 const app = express();
-const swaggerUi = require("swagger-ui-express");
+//const swaggerUi = require("swagger-ui-express");
 const bodyParser = require('body-parser')
 const apiRouter = require("../routes")
-const  swaggerDocument = require("../swagger/swaggerDocument.json");
+//const  swaggerDocument = require("../swagger/swaggerDocument.json");
 
 
 //import apiRouter from "../routes"
-var cors = require('cors');
-var path = require('path');
+const cors = require('cors');
+const path = require('path');
 
 const schema = require("../appolo/schema");
 const resolvers = require("../appolo/resolvers");
@@ -31,11 +31,11 @@ graphQlServer.applyMiddleware({ app, path: "/graphql" });
 
 
 //app.use('/static', express.static(path.join(__dirname, 'public')))
-
+app.use(cors());
 app.use('/uploads', express.static('./uploads'));
 app.use(bodyParser.json());
 //app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
-app.use(cors());
+
 app.use('/api/v1', apiRouter);
 
 
