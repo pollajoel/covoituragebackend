57c83d821711796159ca52bb9cf8fb37acff71c1
pollajoel <pollajoel2017@gmail.com>
2021-06-02 22:42:23 +0200

    add

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,6 +7,7 @@ const AuthJwt = require("../middleware/secureRoute.middleware")
 router.post("/users",users.register)
 router.get("/users/:id",users.user)
 router.get("/users/",users.users)
+router.post("/login",users.login);
 //router.post("/userDelete",users.Delete)
 router.get("/logout",AuthJwt.authenticateJWT,users.logout)
 router.put("/user/:id", users.update);
